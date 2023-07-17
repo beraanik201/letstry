ce1768574e02c8b88ddbb4aea1d718411d42f87a
Anik Bera <beraanik201@gmail.com>
2023-07-17 12:17:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+import "./App.css";
+import "./index.css";
+
+function App() {
+  //   const sw =
+  // "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+  //   console.log(sw)
+  return (
+    <div className="App">
+      <div className="intro">
+      <h1>
+        Hello,my name is Anik Bera. I did a B.Tech. in computer science and
+        engineering from Techno India University, Kolkata. I passed out in 2023.
+        I have knowledge of Java,HTML,CSS, Javascript, and React JS. I have made
+        a food delivery website using React JS. I am self-motivated and a quick
+        learner. I want to explore and deep-dive into these technologies. As a
+        fresher, I want to learn new technologies and problem-solving abilities.
+      </h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
